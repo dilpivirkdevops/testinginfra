5fd5538faf5a56895ad90ffa99703672fa71a4c6
dilpivirkdevops <80132714+dilpivirkdevops@users.noreply.github.com>
2021-03-06 14:29:03 -0500

    Create Security.tf

---
+++ Security.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "web_access" {
+  name        = "sg_web_access"
+  description = "Very simple security group to allow HTTP & HTTPS access."
+
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "sg_ssh_access"
+  description = "Security group to allow SSH access."
+
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
